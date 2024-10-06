98795825b1f5c3135e7e25c65a711fc28bea3db7
Elizabeth Abrego <an0174032023@unab.edu.sv>
2024-10-05 18:21:16 -0600

    Guía 4 'Controladores', finalizada.

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+718719afea042d3206daf9dd1320c7f8ec41cd88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6b8b73401195854cbbfb9e3ee9b05a9ce1293d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
