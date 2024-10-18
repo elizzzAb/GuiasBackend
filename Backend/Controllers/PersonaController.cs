d9e935511356eefdf06e1e757ac079a00df6cacb
Elizabeth Abrego <an0174032023@unab.edu.sv>
2024-10-18 15:27:53 -0600

    Guía 10: Respuesta ActionResult

--- Backend/Controllers/PersonaController.cs
+++ Backend/Controllers/PersonaController.cs
@@ -11,8 +11,9 @@ namespace Backend.Controllers
         public List<PersonaDatos> GetPersonaDatos() => Repository.persona;
 
 
-        [HttpGet("{id}")]
-        public PersonaDatos GetPersonaDatos(int id) => Repository.persona.FirstOrDefault(p => p.id == id);
+        //[HttpGet("{id}")]
+        //public PersonaDatos GetPersonaDatos(int id) => Repository.persona.FirstOrDefault(p => p.id == id);
+
 
         //[HttpGet("search/{search}")]
         //public List<PersonaDatos> Get(string search) => Repository.persona.Where( p => p.name.Contains(search)).ToList();
@@ -20,6 +21,18 @@ namespace Backend.Controllers
         [HttpGet("search/{search}")]
         public List<PersonaDatos> Get(string search) => 
             Repository.persona.Where(p => p.name.ToUpper().Contains(search.ToUpper())).ToList();
+
+
+        [HttpGet("{id}")]
+        public ActionResult<PersonaDatos> Get(int id)
+        {
+            var persona = Repository.persona.FirstOrDefault(p => p.id == id);
+            if (persona == null)
+            {
+                return NotFound(); //'NotFound()' -> Esto nos ha dado un error
+            }
+            return Ok(persona);
+        }
 
 
     }
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fada1cbd91e66626e695a6bc49bb3accf7a6af1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b41aedb7796d4e14efa6425f162798490b7d7ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
